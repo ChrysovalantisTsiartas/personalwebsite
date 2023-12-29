137e1387c472ea212b544aeebe1a169fde621781
Chrysovalantis Tsiartas <chrysovalantis2@gmail.com>
2023-12-29 21:21:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const typedText = document.getElementById('typed-text');
-    const typedDate = document.getElementById('typed-date');
     const name = "CHRYSOVALANTIS TSIARTAS"; // Replace with your actual name
-    const date= "04 DECEMBER 2002";
 
     // Set the initial text to an empty string
     typedText.innerHTML = name;
-    typedText.innerHTML = date;
 
     // Function to simulate typing effect
     function typeEffect() {
-        typedText.innerHTML = "";
         typedText.innerHTML = "";
         for (let i = 0; i < name.length; i++) {
             setTimeout(function() {
@@ -19,11 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    setTimeout(function() {
-                typeEffect(date, typedDate);
-            }, 90 * name.length + 1000); // Add a delay of 1000 milliseconds (1 second)
-        });
     // Call the typing function
     typeEffect();
-    
 });
